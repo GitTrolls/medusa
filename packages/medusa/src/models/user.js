8a790277bc9620b7c2361dd433e5d098b3c704e7
GitTrolls <theaaronbyte@gmail.com>
2020-02-25 12:06:57 +0100

    Import missing mongoose module

--- packages/medusa/src/models/user.js
+++ packages/medusa/src/models/user.js
@@ -2,14 +2,15 @@
  * models/user.js
  *
  ******************************************************************************/
+import mongoose from "mongoose"
 import { BaseModel } from "medusa-interfaces"
 
 class UserModel extends BaseModel {
   static modelName = "User"
   static schema = {
-    name: { type: String, required: true },
     email: { type: String, required: true },
     passwordHash: { type: String, required: true },
+    metadata: { type: mongoose.Schema.Types.Mixed, default: {} },
   }
 }
 
