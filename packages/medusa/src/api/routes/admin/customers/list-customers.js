604047cd82420481fcb3f0a4f275064164f7ed4a
GitTrolls <theaaronbyte@gmail.com>
2020-08-21 22:53:15 +0200

    Secure cookies in production

--- packages/medusa/src/api/routes/admin/customers/list-customers.js
+++ packages/medusa/src/api/routes/admin/customers/list-customers.js
@@ -1,15 +1,9 @@
 export default async (req, res) => {
+  const selector = {}
+
   try {
     const customerService = req.scope.resolve("customerService")
-    const queryBuilderService = req.scope.resolve("queryBuilderService")
-
-    const query = queryBuilderService.buildQuery(req.query, [
-      "email",
-      "first_name",
-      "last_name",
-    ])
-
-    const customers = await customerService.list(query)
+    const customers = await customerService.list(selector)
 
     res.json({ customers })
   } catch (error) {
