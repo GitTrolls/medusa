61cf4a37cd144fc20bc0e53787b249180e0d44eb
GitTrolls <theaaronbyte@gmail.com>
2020-09-03 08:44:31 +0200

    fix: debug

--- packages/medusa/src/services/query-builder.js
+++ packages/medusa/src/services/query-builder.js
@@ -38,7 +38,7 @@ class QueryBuilderService extends BaseService {
     if (_.isEmpty(search)) return
 
     const searchQuery = searchProperties.map(s => ({
-      [s]: new RegExp(search.q, "i"),
+      [s]: new RegExp(search.q),
     }))
 
     return { $or: searchQuery }
