150df5548a03c9bf61da942b6d48a981ec057848
GitTrolls <theaaronbyte@gmail.com>
2021-02-17 10:40:48 +0100

    fix: use parallel relation fetching (#173)

--- packages/medusa/src/services/customer.js
+++ packages/medusa/src/services/customer.js
@@ -3,7 +3,6 @@ import Scrypt from "scrypt-kdf"
 import _ from "lodash"
 import { Validator, MedusaError } from "medusa-core-utils"
 import { BaseService } from "medusa-interfaces"
-import { Brackets } from "typeorm"
 
 /**
  * Provides layer to manipulate customers.
@@ -111,6 +110,7 @@ class CustomerService extends BaseService {
     const token = jwt.sign(payload, secret)
     // Notify subscribers
     this.eventBus_.emit(CustomerService.Events.PASSWORD_RESET, {
+      id: customerId,
       email: customer.email,
       first_name: customer.first_name,
       last_name: customer.last_name,
@@ -128,34 +128,7 @@ class CustomerService extends BaseService {
       this.customerRepository_
     )
 
-    let q
-    if ("q" in selector) {
-      q = selector.q
-      delete selector.q
-    }
-
     const query = this.buildQuery_(selector, config)
-
-    if (q) {
-      const where = query.where
-
-      delete where.email
-      delete where.first_name
-      delete where.last_name
-
-      query.where = qb => {
-        qb.where(where)
-
-        qb.andWhere(
-          new Brackets(qb => {
-            qb.where(`email ILIKE :q`, { q: `%${q}%` })
-              .orWhere(`first_name ILIKE :q`, { q: `%${q}%` })
-              .orWhere(`last_name ILIKE :q`, { q: `%${q}%` })
-          })
-        )
-      }
-    }
-
     return customerRepo.find(query)
   }
 
@@ -320,6 +293,7 @@ class CustomerService extends BaseService {
 
       const {
         email,
+        password,
         password_hash,
         billing_address,
         metadata,
@@ -340,6 +314,10 @@ class CustomerService extends BaseService {
 
       for (const [key, value] of Object.entries(rest)) {
         customer[key] = value
+      }
+
+      if (password) {
+        customer.password_hash = await this.hashPassword_(password)
       }
 
       const updated = await customerRepository.save(customer)
