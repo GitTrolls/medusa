a6fd6134188c8cb16fcfc8ec98dfbca9206897df
GitTrolls <theaaronbyte@gmail.com>
2021-09-20 10:56:15 +0100

    feat: rma shipping option + unit tests

--- www/docs/sidebars.js
+++ www/docs/sidebars.js
@@ -43,10 +43,10 @@ module.exports = {
           type: "doc",
           id: "tutorial/adding-custom-functionality",
         },
-        // {
-        //   type: "doc",
-        //   id: "tutorial/linking-your-local-project-with-medusa-cloud",
-        // },
+        {
+          type: "doc",
+          id: "tutorial/linking-your-local-project-with-medusa-cloud",
+        },
       ],
     },
     {
@@ -75,10 +75,6 @@ module.exports = {
           type: "doc",
           id: "how-to/setting-up-a-nextjs-storefront-for-your-medusa-project",
         },
-        {
-          type: "doc",
-          id: "how-to/create-medusa-app",
-        },
       ],
     },
     {
@@ -88,28 +84,6 @@ module.exports = {
         {
           type: "doc",
           id: "guides/fulfillment-api",
-        },
-        {
-          type: "doc",
-          id: "guides/checkouts",
-        },
-        {
-          type: "doc",
-          id: "guides/carts-in-medusa",
-        },
-      ],
-    },
-    {
-      type: "category",
-      label: "Deploy",
-      items: [
-        {
-          type: "doc",
-          id: "how-to/deploying-on-heroku",
-        },
-        {
-          type: "doc",
-          id: "how-to/deploying-admin-on-netlify",
         },
       ],
     },
