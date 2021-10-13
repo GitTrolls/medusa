069a5076fdf58523b154572ac8001c2157b7036e
GitTrolls <theaaronbyte@gmail.com>
2021-10-13 13:45:50 -0400

    fixed linting errors on packages/medusa/src/services/user.js #531 (#543)

--- www/docs/sidebars.js
+++ www/docs/sidebars.js
@@ -69,10 +69,6 @@ module.exports = {
               type: "doc",
               id: "how-to/headless-ecommerce-store-with-gatsby-contentful-medusa",
             },
-            {
-              type: "doc",
-              id: "how-to/making-your-store-more-powerful-with-contentful",
-            },
           ],
         },
         {
@@ -118,10 +114,6 @@ module.exports = {
         {
           type: "doc",
           id: "how-to/deploying-on-heroku",
-        },
-        {
-          type: "doc",
-          id: "how-to/deploying-on-digital-ocean",
         },
         {
           type: "doc",
