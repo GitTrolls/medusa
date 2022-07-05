865a1069b43301745239d65517c1394ed2de9609
GitTrolls <theaaronbyte@gmail.com>
2022-07-05 09:51:23 +0200

    chore(release): Publish

--- packages/medusa/src/repositories/sales-channel.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm"
-import { SalesChannel } from "../models/sales-channel"
-
-@EntityRepository(SalesChannel)
-export class SalesChannelRepository extends Repository<SalesChannel> {}
