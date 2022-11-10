474506a7dea8fc1956e0db92d7487276dc2c38b2
GitTrolls <theaaronbyte@gmail.com>
2022-11-10 14:04:34 +0200

    docs: temporary fix for memory error that occurs during build (#2584)

--- packages/medusa/src/repositories/publishable-api-key.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository, Repository } from "typeorm"
-
-import { PublishableApiKey } from "../models/publishable-api-key"
-
-@EntityRepository(PublishableApiKey)
-export class PublishableApiKeyRepository extends Repository<PublishableApiKey> {}
